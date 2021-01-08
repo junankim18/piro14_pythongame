dd16f002b6024ccaf0e27ae211d7eaa3d083fa73
Junan Kim <junankim18@naver.com>
2021-01-08 22:13:51 +0900

    총알 움직임

--- pythongame.py
+++ pythongame.py
@@ -29,25 +29,26 @@ class Ball:
 
     def bul_move(self):         # Bullet을 이동시키는 함수
         self.canvas.move(self.id, self.xspeed, self.yspeed)
-        (x1, y1, x2, y2) = self.canvas.coords(self.id)   # 공의 현재 위치를 얻는다.
+        (x1, y1, x2, y2) = self.canvas.coords(self.id)
         (self.x, self.y) = (x1, y1)
-        if x1 <= 0 or x2 >= WIDTH:  # 공의 x좌표가 음수이거나 x좌표가 오른쪽 경계를 넘으면
-            self.xspeed = - self.xspeed      # 속도의 부호를 반전시킨다.
-        if y1 <= 0 or y2 >= HEIGHT:  # 공의 x좌표가 음수이거나 x좌표가 오른쪽 경계를 넘으면
-            self.yspeed = - self.yspeed      # 속도의 부호를 반전시킨다.
+        if x1 <= 0 or x2 >= WIDTH:
+            self.xspeed = - self.xspeed
+        if y1 <= 0 or y2 >= HEIGHT:
+            self.yspeed = - self.yspeed
+
+    def ship_move(self):         # Bullet을 이동시키는 함수
+        self.canvas.move(self.id, self.xspeed, self.yspeed)
+        (x1, y1, x2, y2) = self.canvas.coords(self.id)
+        (self.x, self.y) = (x1, y1)
+        if x1 <= 0 or x2 >= WIDTH:
+            self.xspeed = - self.xspeed
+        if y1 <= 0 or y2 >= HEIGHT:
+            self.yspeed = - self.yspeed
 
 
 # 생성된 포탄을 저장하는 리스트
 bullets = []
 lengths = []
-
-chance = 0
-
-
-def fire(event):  # 이벤트를 처리하는 함수
-    global chance
-    bullets.append(Ball(canvas, 'white', 10, 150, 250, 15, 0))
-    chance += 1
 
 
 paused = False
@@ -71,12 +72,38 @@ def stop():
     time.sleep(1)
 
 
-i = 0
-# 윈도우를 생성한다.
 window = Tk()
 window.title('GreenShooter')  # 우리 게임 이름 정함
 window.resizable(False, False)  # 게임창 사이즈 임의로 못바꾸게
 canvas = Canvas(window, width=WIDTH, height=HEIGHT, relief='solid', bd=2)
+
+
+# 우리 우주선과 외계 우주선을 생성한다.
+a = random.randrange(-49, 50)/10
+b = random.randrange(-49, 50)/10
+c = random.randrange(-99, 100)/20
+if a < 0:
+    aa = -3
+else:
+    aa = 3
+if b < 0:
+    bb = -3
+else:
+    bb = 3
+spaceship = Ball(canvas, 'green', 100, 100, 200, 0, 0)
+enemy = Ball(canvas, 'red', 100, 500, 200, a + aa, b + bb)
+chance = 0
+d = spaceship.y
+
+
+def fire(event):  # 이벤트를 처리하는 함수
+    global chance
+    bullets.append(Ball(canvas, 'white', 10, 150, d, 20, 0))
+    chance += 1
+
+
+i = 0
+# 윈도우를 생성한다.
 canvas.grid(row=0, column=0, columnspan=1)
 canvas.bind('<Button-1>', fire)
 l1 = Label(window, text='점수', fg='green', relief='groove')
@@ -93,23 +120,22 @@ b1 = Button(window, text='stop for 2sec', command=stop, relief='groove')
 b1.grid(row=3, column=3)
 canvas.bind('<Button-3>', is_paused)
 
-# 우리 우주선과 외계 우주선을 생성한다.
-a = random.randrange(-99, 100)/10
-b = random.randrange(-99, 100)/10
-spaceship = Ball(canvas, 'green', 100, 100, 200, 0, 0)
-enemy = Ball(canvas, 'red', 100, 500, 200, a, b)
 
 # 리스트에 저장된 각각의 객체를 이동시킨다.
 while True:
     # canvas.bind('<Button-3>', is_paused)
-    e2.delete(0, END)
-    e2.insert(0, 20-chance)
     while True:
+        print(spaceship.y)
+        print(d)
+        time.sleep(0.1)
+        e2.delete(0, END)
+        e2.insert(0, 20-chance)
         if paused:
             enemy.xspeed = 0
             enemy.yspeed = 0
             break
         else:
+
             for bullet in bullets:
                 bullet.bul_move()
                 (a, b) = (bullet.x+5, bullet.y+5)
@@ -130,9 +156,10 @@ while True:
                     canvas.delete(bullet.id)
                     bullets.remove(bullet)
             enemy.move()
+            spaceship.ship_move()
             window.update()
             time.sleep(0.03)
-            if chance == 21:
+            if chance == 100:
                 break
     # enemy.xspeed = 3
     # enemy.yspeed = 3
@@ -164,6 +191,6 @@ while True:
     time.sleep(0.03)
     e2.delete(0, END)
     e2.insert(0, 20-chance)
-    # print(chance)
-    if chance == 21:
+    print(spaceship.y)
+    if chance == 100:
         break
